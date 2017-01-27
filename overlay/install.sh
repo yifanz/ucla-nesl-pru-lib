e98241c2472db6d6dd496e5d8f6f9ff3f99aba68
Yi-Fan Zhang <yifanzhang@engineering.ucla.edu>
2017-01-27 04:54:46 +0000

    Update overlay install script
    Newer kernels put the bone cape manager in another dir

--- overlay/install.sh
+++ overlay/install.sh
@@ -4,6 +4,15 @@ NAME=NESL-PRU
 DTBO=$NAME-00A0.dtbo
 DTS=$NAME.dts
 
+CAPE_MGR="/sys/devices/platform/bone_capemgr/slots"
+CAPE_MGR_OLD="/sys/devices/bone_capemgr.9/slots"
+PIN_CONFIG=false
+
+if [ ! -e $CAPE_MGR ]; then
+    CAPE_MGR=$CAPE_MGR_OLD
+    PIN_CONFIG=true
+fi
+
 cd ${0%/*}
 
 echo "Compiling the overlay from .dts to .dtbo"
@@ -17,10 +26,16 @@ fi
 
 if [ $? -eq 0 ]; then
     echo "Activating PRU overlay"
-    echo $NAME > /sys/devices/bone_capemgr.9/slots
+    echo $NAME > $CAPE_MGR
 fi
 
-if cat /sys/devices/bone_capemgr.9/slots | grep -q cape-universal; then
-    echo "Activating cape-universal overlay"
-    config-pin overlay cape-universal
+if [ "$PIN_CONFIG" = true ]; then
+    if cat $CAPE_MGR | grep -q cape-universal; then
+        echo "Activating cape-universal overlay"
+        config-pin overlay cape-universal
+    fi
 fi
+
+sleep 1
+
+cat $CAPE_MGR
