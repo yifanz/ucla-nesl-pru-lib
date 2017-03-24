ca31bdc9305846264459ebef7c5743d689a6da9b
Yi-Fan Zhang <yifanz@yahoo.com>
2017-03-23 18:08:57 -0700

    Update default values for usb-net-forwarding

--- scripts/host-usb-net-forwarding.sh
+++ scripts/host-usb-net-forwarding.sh
@@ -6,9 +6,11 @@
 #
 
 # Network interface between host <--> internet
-HOST_NET_IFACE="eth0"
+HOST_NET_IFACE="ens33"
+#HOST_NET_IFACE="eth0"
 # Network interface between host <--> BBB
-HOST_BBB_IFACE="eth2"
+HOST_BBB_IFACE="enx8030dc55737f"
+#HOST_BBB_IFACE="eth2"
 
 sudo iptables --table nat --append POSTROUTING --out-interface $HOST_NET_IFACE -j MASQUERADE
 sudo iptables --append FORWARD --in-interface $HOST_BBB_IFACE -j ACCEPT
